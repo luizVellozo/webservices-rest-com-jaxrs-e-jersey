7450e019354c748b7b3d59e8a3d2de79b8fa4c81
luizVellozo <felipe1740@gmail.com>
2015-07-27 00:22:19 -0300

    Adicionando Response 200 nos recursos POST

--- src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -1,12 +1,21 @@
 package br.com.alura.loja.resource;
 
+import java.net.URI;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.dao.ProjetoDAO;
+import br.com.alura.loja.modelo.Carrinho;
 import br.com.alura.loja.modelo.Projeto;
 
 @Path("projetos")
@@ -27,5 +36,16 @@ public class ProjetoResource {
 		Projeto projeto = new ProjetoDAO().busca(id);
 		return projeto.toJson();
 	}
+	
+	//Podemos testar Post com o CURL passando o parrametro -d
+	@POST
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response adiciona(String conteudo) {
+		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+        new ProjetoDAO().adiciona(projeto);
+        
+        URI location = URI.create("/projetos/"+projeto.getId());
+        return Response.created(location).build();
+    }
 	
 }
--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -4,7 +4,11 @@ import static org.junit.Assert.*;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
 import org.junit.Before;
@@ -13,6 +17,7 @@ import org.junit.Test;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.modelo.Projeto;
 import br.com.alura.loja.resource.Servidor;
 
@@ -42,7 +47,6 @@ public class ClienteTest {
 	
 	@Test
 	public void testeQueTrazProjetoDeId1() {
-
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080");
 		String conteudo = target.path("/projetos/1").request().get(String.class);
@@ -51,6 +55,34 @@ public class ClienteTest {
 		
 		assertEquals("Minha loja", projeto.getNome());
 		assertEquals(projeto.getAnoDeInicio(),2014);
-
+	}
+	
+	@Test
+	public void testeQueAdicionaUmCarrinhoViaPost() {
+		Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        
+        //precisamos criar um carrinho e transforma-lo em XML para realizar o post:
+        Carrinho carrinho = new Carrinho();
+        carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+        carrinho.setRua("Rua Vergueiro");
+        carrinho.setCidade("Sao Paulo");
+        String xml = carrinho.toXML();
+        
+//        //Agora que temos o XML e sabemos que o media type 
+//        que enviaremos é application/xml, 
+//        precisamos representar isso de alguma maneira. 
+//        Utilizaremos a classe Entity do próprio JAX-RS, para criar tal representação
+        
+        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+        
+        //Teste se a requisisão obteve sucesso:201
+        Response response = target.path("/carrinhos").request().post(entity);
+        String location = response.getHeaderString("Location");
+        assertEquals(201, response.getStatus());
+        
+        //Testa se o resource gerado é o mesmo que o enviado 
+        String conteudo = client.target(location).request().get(String.class);
+        assertTrue(conteudo.contains("Tablet") && conteudo.contains("Rua Vergueiro"));
 	}
 }
